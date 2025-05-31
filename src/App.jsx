1b491eea09fac48df0ee027ec1e1a4b136043cd0
minmmmmin <kotomi.hinahima@gmail.com>
2025-05-31 14:37:03 +0900

    散布図をスケールする

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import * as d3 from "d3";
 
 function App() {
-  const scale = d3.scaleLinear();
-  scale.domain([100, 500]);
-  scale.range([10, 350]);
-  console.log(scale(100));
-  console.log(scale(300));
-  console.log(scale(500));
+  const dataset = [
+    [5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
+    [410, 12], [475, 44], [25, 67], [85, 21], [220, 88]
+  ];
+  const w = 500;
+  const h = 500;
+  const xScale = d3.scaleLinear()
+  .domain([0,d3.max(dataset,function(d){return d[0];})])
+  .range([0,w]);
+  const yScale = d3.scaleLinear()
+  .domain([0, d3.max(dataset, function(d) { return d[1]; })])
+  .range([0, h]);
 }
 
 export default App;
