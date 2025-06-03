be9bd6e84afff0095ee9013cd448c9cad8e010de
minmmmmin <kotomi.hinahima@gmail.com>
2025-06-03 17:42:37 +0900

    フッターの追加

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>Iris scatter plot is retrieved from the Iris dataset</p>
+        <p>
+          <a href="https://archive.ics.uci.edu/dataset/53/iris">Donate to Iris dataset</a>
+        </p>
+      </div>
+    </footer>
+  );
+}
