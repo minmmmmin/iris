83db7ca4a379d7864231e9e30817eea5980b2473
minmmmmin <kotomi.hinahima@gmail.com>
2025-06-04 20:31:49 +0900

    エラーを直した

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Dropdown from "./components/ScatterPlot/Dropdown";
 import ScatterPlot from "./components/ScatterPlot/ScatterPlot.jsx";
 
 function App() {
   return (
     <div>
       <Header />
-      <Dropdown />
       <ScatterPlot />
       <Footer />
     </div>
