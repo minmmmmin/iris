5e9c1b6dc04c231f3d9b116e928d032f53d31325
minmmmmin <kotomi.hinahima@gmail.com>
2025-06-05 02:51:29 +0900

    セレクトボックスに変更

--- src/components/ScatterPlot/Dropdown.jsx
+++ src/components/ScatterPlot/Dropdown.jsx
@@ -1,47 +1,20 @@
 import { useState } from "react";
 
-export default function Dropdown({ options, selected, setSelected }) {
-  const [open, setOpen] = useState(false);
-
-  const handleSelect = (option) => {
-    setSelected(option);
-    setOpen(false);
-  };
-
+export default function Dropdown({
+  options,
+  selected,
+  setSelected,
+  isFocused,
+}) {
   return (
-    <div className={`dropdown ${open ? "is-active" : ""}`}>
-      <div className="dropdown-trigger">
-        <button
-          className="button"
-          aria-haspopup="true"
-          aria-controls="dropdown-menu"
-          onClick={() => setOpen(!open)}
-        >
-          <span>{selected}</span>
-          <span className="icon is-small">
-            <i className="fas fa-angle-down" aria-hidden="true"></i>
-          </span>
-        </button>
-      </div>
-      <div className="dropdown-menu" role="menu">
-        <div className="dropdown-content">
-          {options.map((option) => (
-            <a
-              href="#"
-              key={option}
-              className={`dropdown-item ${
-                option === selected ? "is-active" : ""
-              }`}
-              onClick={(e) => {
-                e.preventDefault();
-                handleSelect(option);
-              }}
-            >
-              {option}
-            </a>
-          ))}
-        </div>
-      </div>
+    <div className={`select ${isFocused ? "has-focused-border" : ""}`}>
+      <select value={selected} onChange={(e) => setSelected(e.target.value)}>
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
