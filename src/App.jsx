1726a202fa79056a51e175e4250a72a21b9d24e1
minmmmmin <kotomi.hinahima@gmail.com>
2025-06-04 16:08:02 +0900

    ドロップダウンの追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Dropdown from "./components/Dropdown";
 
 function App() {
   return (
     <div>
       <Header />
+      <Dropdown />
       <Footer />
     </div>
   );
